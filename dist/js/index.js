09aad26be16d4bf0410e2cda80ced636e7f2200a
steffanturanjanin <stefan@turanjanin.net>
2021-03-03 01:37:55 +0100

    Delete unused homepage.js file

--- dist/js/index.js
+++ dist/js/index.js
@@ -6,14 +6,6 @@ toggleSubMenuEventListener();
 const header = document.getElementsByTagName('header')[0];
 
 window.addEventListener('resize', function(event) {
-    // if (event.target.screen.width > 860 && header.classList.contains('fix-header')) {
-    //     header.classList.remove('fix-header');
-    // }
-
-    // if (event.target.screen.width < 860 && header.classList.contains('open')) {
-    //     header.classList.add('fix-header');
-    // } 
-
 
     if (event.target.screen.width > 800 && document.body.style.overflow == 'hidden') {
         document.body.style.overflow = 'scroll';
